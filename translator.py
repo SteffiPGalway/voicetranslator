39cd303f1500f9759f971a6b7b97c33a21bf09c8
SteffiPGalway <steffipoliwoda@gmail.com>
2025-02-17 20:06:04 +0100

    enable user to enter api key

--- translator.py
+++ translator.py
@@ -1,12 +1,16 @@
 #import packages
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 from openai import OpenAI
 import streamlit as st
 import os
 
-load_dotenv()
+# load_dotenv()
 
-api_key = os.getenv('OPENAI_API_KEY')
+OpenAI_API_KEY=input("Enter your OpenAI API Key")
+
+# api_key = os.getenv('OPENAI_API_KEY')
+
+api_key=OpenAI_API_KEY
 
 client = OpenAI()
 
@@ -22,4 +26,3 @@ if audio_value:
 
   transcript_text = transcript.text
   st.write(transcript_text)
-  
